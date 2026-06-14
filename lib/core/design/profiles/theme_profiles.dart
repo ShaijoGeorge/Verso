384b496ecbbd57c2ab640e088ae2044e933d9a41
ShaiGeorge <shaijokuttikkatt@gmail.com>
2026-06-15 01:17:46 +0530

    fix: temporarily hid themes

--- lib/core/design/profiles/theme_profiles.dart
+++ lib/core/design/profiles/theme_profiles.dart
@@ -1,13 +1,13 @@
 import 'package:verso/core/design/profiles/candy_pop_profile.dart';
 import 'package:verso/core/design/profiles/current_profile.dart';
-import 'package:verso/core/design/profiles/illuminated_manuscript_profile.dart';
-import 'package:verso/core/design/profiles/malabar_chapel_profile.dart';
+// import 'package:verso/core/design/profiles/illuminated_manuscript_profile.dart';
+// import 'package:verso/core/design/profiles/malabar_chapel_profile.dart';
 import 'package:verso/core/design/profiles/theme_profile.dart';
 
 export 'candy_pop_profile.dart';
 export 'current_profile.dart';
-export 'illuminated_manuscript_profile.dart';
-export 'malabar_chapel_profile.dart';
+// export 'illuminated_manuscript_profile.dart';
+// export 'malabar_chapel_profile.dart';
 export 'theme_profile.dart';
 export 'verso_palette.dart';
 
@@ -17,8 +17,8 @@ abstract final class ThemeProfiles {
 
   static final List<ThemeProfile> all = [
     currentProfile,
-    illuminatedManuscriptProfile,
-    malabarChapelProfile,
+    // illuminatedManuscriptProfile,
+    // malabarChapelProfile,
     candyPopProfile,
   ];
 
